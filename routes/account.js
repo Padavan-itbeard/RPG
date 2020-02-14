ffe959ca1329cbd8c3def1409e0f0ad2032e1f83
Aleksandr Ivanov <aleksand7@yandex.ru>
2020-02-14 15:05:59 +0300

    pre relise popytca 2

--- routes/account.js
+++ routes/account.js
@@ -4,33 +4,19 @@ const { Player, Team, Charact, Item } = require('../models/models');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
-// route.get('/', async (req, res) => {
-//   res.render('account')
-// });
-
 route.post('/', async (req, res) => {
-  // if (admin) res.redirect('/admin');
-  const {username, password} = req.body
-  // console.log(username);
-  
-  const player = await Player.findOne({ login: username}).populate('charact').populate('inventory');
-  console.log(username,player); 
-  if( await bcrypt.compare(password, player.password )) {
+  const { username, password } = req.body
+  const player = await Player.findOne({ login: username }).populate('charact').populate('inventory');
+  console.log(username, player);
+  if (await bcrypt.compare(password, player.password)) {
     const admin = await Team.findOne({ commander: player._id });
-    if(admin){ 
-      res.render('admin', {player})
-    }else{
-    const players = await Team.findOne({players: player._id}).populate('players').populate('storage');
-    res.render('account', { player})
-  }
+    if (admin) {
+      res.render('admin', { player })
+    } else {
+      const players = await Team.findOne({ players: player._id }).populate('players').populate('storage');
+      res.render('account', { player })
+    }
   }
 });
-
-// route.get('/', async (req,res)=>{
-// const player = await Player.findOne({_id: req.query.id}).populate('players').populate('storage');
-// res.render('account', { player})
-
-// })
 
 module.exports = route;
--- routes/boss.js
+++ routes/boss.js
@@ -11,18 +11,21 @@ route.get('/:id', async (req, res) => {
 });
 
 route.post('/addCh', async (req, res) => {
+ // console.log('\n\n\n\n\n\n\n\n');
+  
   const { nameCh, nameTeam } = req.body;
   const team = await Team.findOne({ name: nameTeam });
   team.players.forEach(async (id) => {
-    const pl = await Player.findById(id).populate('players');
+    const pl = await Player.findById(id).populate('charact'); //.populate();
     const ch = new Charact({
       name: nameCh,
       value: 0
     });
     //pl.charact.reduce((i, obj) => {(obj[nameCh] !== un7defined) ? 1 : 0 }, 0);
-    //console.log(pl);
+    // console.log(pl.charact);
+    // console.log('\n\n\n\n\n\n\n\n');
+    // console.log('>>>>>>',pl.charact.filter(obj => obj["Int"])) //((i, obj) => {(obj[nameCh] !== undefined) ? 1 : 0 }, 0));
     
-    //console.log(pl.charact.filter(obj => obj[nameCh])) //((i, obj) => {(obj[nameCh] !== undefined) ? 1 : 0 }, 0));
     await ch.save();
     await pl.charact.push(ch._id);
     await pl.save();
