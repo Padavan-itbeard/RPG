35fc7efe22395192eeed505825ba9536b2365985
Aleksandr Ivanov <aleksand7@yandex.ru>
2020-02-14 14:50:36 +0300

    pred relise

--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-// route.post('/', async (req, res) => {
+// route.get('/', async (req, res) => {
 //   res.render('account')
 // });
 
@@ -15,7 +15,7 @@ route.post('/', async (req, res) => {
   // console.log(username);
   
   const player = await Player.findOne({ login: username}).populate('charact').populate('inventory');
-  
+  console.log(username,player); 
   if( await bcrypt.compare(password, player.password )) {
     const admin = await Team.findOne({ commander: player._id });
     if(admin){ 
@@ -27,10 +27,10 @@ route.post('/', async (req, res) => {
   }
 });
 
-route.get('/', async (req,res)=>{
-const player = await Player.findOne({_id: req.query.id}).populate('players').populate('storage');
-res.render('account', { player})
+// route.get('/', async (req,res)=>{
+// const player = await Player.findOne({_id: req.query.id}).populate('players').populate('storage');
+// res.render('account', { player})
 
-})
+// })
 
 module.exports = route;
